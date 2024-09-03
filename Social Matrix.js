00138736884267a897c09c8bd31905dbd9347885
Tim Dobson <hello@tdobson.net>
2024-09-03 12:39:58 +0100

    init

---
+++ Social Matrix.js
@@ -0,0 +1,134 @@
+var server = '18.168.242.164';
+var port = 3306;
+var dbName = 'bitnami_wordpress';
+var username = 'gsheets';
+var password = 'eyai4yohF4uX8eeP7phoob';
+var url = 'jdbc:mysql://'+server+':'+port+'/'+dbName;
+
+function readData() {
+ var conn = Jdbc.getConnection(url, username, password);
+ var stmt = conn.createStatement();
+
+
+ //Diet
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Dashboard');
+ var cell = sheet.getRange('B4').getValues();
+ 
+ var results = stmt.executeQuery('select distinct `first_name` "First Name",`nickname` "Facebook Name",`social-menu-choice-one` Starter,`social-menu-choice-two` "Main",`social-menu-choice-three` "Desert", `admin-dietary-requirements` "Dietary Requirements", `admin-diet-allergies-health-extra-info` "Extra Diet Info", `admin-social-requests-notes` "Notes" , order_id  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND pd.status="wc-processing" order by `first_name` ASC');
+  //console.log(results);
+ var metaData=results.getMetaData();
+  var numCols = metaData.getColumnCount();
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Diet');
+ sheet.clearContents();
+ var arr=[];
+  for (var col = 0; col < numCols; col++) {
+   arr.push(metaData.getColumnName(col + 1));
+ }
+  sheet.appendRow(arr);
+ while (results.next()) {
+ arr=[];
+ for (var col = 0; col < numCols; col++) {
+   arr.push(results.getString(col + 1));
+ }
+ sheet.appendRow(arr);
+ }
+
+
+sheet.autoResizeColumns(1, numCols+1);
+//Transport
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Dashboard');
+ var cell = sheet.getRange('B4').getValues();
+ 
+ var results = stmt.executeQuery('select `first_name`,`nickname` fbname, `transport-need-lift`, `transport-will-you-give-lift`, `transport-leaving-location`, `transport-depature-time` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
+  //console.log(results);
+ var metaData=results.getMetaData();
+  var numCols = metaData.getColumnCount();
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Transport');
+ sheet.clearContents();
+ var arr=[];
+  for (var col = 0; col < numCols; col++) {
+   arr.push(metaData.getColumnName(col + 1));
+ }
+  sheet.appendRow(arr);
+ while (results.next()) {
+ arr=[];
+ for (var col = 0; col < numCols; col++) {
+   arr.push(results.getString(col + 1));
+ }
+ sheet.appendRow(arr);
+ }
+
+sheet.autoResizeColumns(1, numCols+1);
+
+
+
+ 
+
+ //Volunteering
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Dashboard');
+ var cell = sheet.getRange('B4').getValues();
+ 
+ var results = stmt.executeQuery('select `first_name`,`nickname` fbname, `admin-can-you-help-social` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
+  //console.log(results);
+ var metaData=results.getMetaData();
+  var numCols = metaData.getColumnCount();
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Volunteering');
+ sheet.clearContents();
+ var arr=[];
+  for (var col = 0; col < numCols; col++) {
+   arr.push(metaData.getColumnName(col + 1));
+ }
+  sheet.appendRow(arr);
+ while (results.next()) {
+ arr=[];
+ for (var col = 0; col < numCols; col++) {
+   arr.push(results.getString(col + 1));
+ }
+ sheet.appendRow(arr);
+ }
+
+sheet.autoResizeColumns(1, numCols+1);
+
+
+ //Buddy
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Dashboard');
+ var cell = sheet.getRange('B4').getValues();
+ 
+ var results = stmt.executeQuery('select distinct `first_name`,`nickname` fbname, `admin-first-timer-question`, `_order_count`  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `_order_count` ASC');
+  //console.log(results);
+ var metaData=results.getMetaData();
+  var numCols = metaData.getColumnCount();
+ var spreadsheet = SpreadsheetApp.getActive();
+ var sheet = spreadsheet.getSheetByName('Buddy');
+ sheet.clearContents();
+ var arr=[];
+  for (var col = 0; col < numCols; col++) {
+   arr.push(metaData.getColumnName(col + 1));
+ }
+  sheet.appendRow(arr);
+ while (results.next()) {
+ arr=[];
+ for (var col = 0; col < numCols; col++) {
+   arr.push(results.getString(col + 1));
+ }
+ sheet.appendRow(arr);
+ }
+
+results.close();
+stmt.close();
+sheet.autoResizeColumns(1, numCols+1);
+
+} 
+
+
+//ScriptApp.newTrigger('readData')
+//.timeBased()
+//.everyMinutes(30)
+//.create();

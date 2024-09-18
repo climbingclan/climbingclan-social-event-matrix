a4e8e0a3686f5d2fd9f4c929e5f40ca6edaee756
Tim Dobson <hello@tdobson.net>
2024-09-18 14:46:20 +0100

    refactor: Explicitly reference first_name column from wp_member_db table

--- Social Matrix.js
+++ Social Matrix.js
@@ -15,7 +15,7 @@ function readData() {
  var sheet = spreadsheet.getSheetByName('Dashboard');
  var cell = sheet.getRange('B4').getValues();
  
- var results = stmt.executeQuery('select distinct `first_name` "First Name",`nickname` "Facebook Name",`social-menu-choice-one` Starter,`social-menu-choice-two` "Main",`social-menu-choice-three` "Desert", `admin-dietary-requirements` "Dietary Requirements", `admin-diet-allergies-health-extra-info` "Extra Diet Info", `admin-social-requests-notes` "Notes" , order_id  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND pd.status="wc-processing" order by `first_name` ASC');
+ var results = stmt.executeQuery('select distinct db.`first_name` "First Name",`nickname` "Facebook Name",`social-menu-choice-one` Starter,`social-menu-choice-two` "Main",`social-menu-choice-three` "Desert", `admin-dietary-requirements` "Dietary Requirements", `admin-diet-allergies-health-extra-info` "Extra Diet Info", `admin-social-requests-notes` "Notes" , order_id  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND pd.status="wc-processing" order by db.`first_name` ASC');
   //console.log(results);
  var metaData=results.getMetaData();
   var numCols = metaData.getColumnCount();
@@ -42,7 +42,7 @@ sheet.autoResizeColumns(1, numCols+1);
  var sheet = spreadsheet.getSheetByName('Dashboard');
  var cell = sheet.getRange('B4').getValues();
  
- var results = stmt.executeQuery('select `first_name`,`nickname` fbname, `transport-need-lift`, `transport-will-you-give-lift`, `transport-leaving-location`, `transport-depature-time` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
+ var results = stmt.executeQuery('select db.`first_name`,`nickname` fbname, `transport-need-lift`, `transport-will-you-give-lift`, `transport-leaving-location`, `transport-depature-time` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
   //console.log(results);
  var metaData=results.getMetaData();
   var numCols = metaData.getColumnCount();
@@ -73,7 +73,7 @@ sheet.autoResizeColumns(1, numCols+1);
  var sheet = spreadsheet.getSheetByName('Dashboard');
  var cell = sheet.getRange('B4').getValues();
  
- var results = stmt.executeQuery('select `first_name`,`nickname` fbname, `admin-can-you-help-social` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
+ var results = stmt.executeQuery('select db.`first_name`,`nickname` fbname, `admin-can-you-help-social` from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `transport-need-lift` ASC');
   //console.log(results);
  var metaData=results.getMetaData();
   var numCols = metaData.getColumnCount();
@@ -101,7 +101,7 @@ sheet.autoResizeColumns(1, numCols+1);
  var sheet = spreadsheet.getSheetByName('Dashboard');
  var cell = sheet.getRange('B4').getValues();
  
- var results = stmt.executeQuery('select distinct `first_name`,`nickname` fbname, `admin-first-timer-question`, `_order_count`  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `_order_count` ASC');
+ var results = stmt.executeQuery('select distinct db.`first_name`,`nickname` fbname, `admin-first-timer-question`, `_order_count`  from wp_member_db db LEFT JOIN wp_order_product_customer_lookup pd on pd.user_id = db.id where product_id=' + cell + ' AND status="wc-processing" order by `_order_count` ASC');
   //console.log(results);
  var metaData=results.getMetaData();
   var numCols = metaData.getColumnCount();
